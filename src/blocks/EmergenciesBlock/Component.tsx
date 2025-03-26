b3fb143595640fc6ead938bbe29e5a938c9b724a
christdevtech <michael@christdev.com>
2025-03-26 23:00:13 +0100

    Completed the Emergencies Block

--- src/blocks/EmergenciesBlock/Component.tsx
+++ src/blocks/EmergenciesBlock/Component.tsx
@@ -6,6 +6,8 @@ import { cn } from '@/utilities/ui'
 
 import type { EmergenciesBlock as EmergenciesBlockType } from '@/payload-types'
 import RichText from '@/components/RichText'
+import { Media } from '@/components/Media'
+import Link from 'next/link'
 
 type Props = EmergenciesBlockType & {
   className?: string
@@ -16,35 +18,47 @@ export const EmergenciesBlock: React.FC<Props> = (props) => {
   const {
     title,
     titleClasses,
+    subtitle,
+    subtitleClasses,
     mainText,
     mainTextClasses,
+    subText,
+    subTextClasses,
     backgroundColor,
     padding,
     emergencyCases,
     suicideHotline,
     className,
+    localizedResources,
   } = props
 
   return (
     <section className={cn(backgroundColor, padding, className)}>
       <div className="container">
-        <h2 className={cn(titleClasses)}>{title}</h2>
+        <h2 className={cn(titleClasses)} dangerouslySetInnerHTML={{ __html: title }} />
         <RichText
           data={mainText}
           enableProse={false}
           enableGutter={false}
           className={cn(mainTextClasses)}
         />
+        <h3 className={cn(subtitleClasses)} dangerouslySetInnerHTML={{ __html: subtitle }} />
+        <RichText
+          data={subText}
+          enableProse={false}
+          enableGutter={false}
+          className={cn(subTextClasses)}
+        />
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16">
           {/* Emergency Cases */}
           <div>
             <h3 className={cn(emergencyCases?.titleClasses)}>{emergencyCases?.title}</h3>
-            <hr className="mb-6 mt-2 border-t border-teal-100" />
+            <hr className="mb-6 mt-2 border-t border-teal-300" />
             <ul className="space-y-4">
               {emergencyCases?.cases?.map((item, index) => (
                 <li key={index} className="flex items-start">
                   <span className="inline-block w-4 h-4 rounded-full bg-teal-300 mr-4 mt-1 flex-shrink-0"></span>
-                  <span className="text-white text-lg md:text-xl">{item.text}</span>
+                  <span className="text-white text-xl md:text-2xl">{item.text}</span>
                 </li>
               ))}
             </ul>
@@ -53,17 +67,38 @@ export const EmergenciesBlock: React.FC<Props> = (props) => {
           {/* Suicide Hotline */}
           <div>
             <h3 className={cn(suicideHotline?.titleClasses)}>{suicideHotline?.title}</h3>
-            <hr className="mb-6 mt-2 border-t border-teal-100" />
+            <hr className="mb-6 mt-2 border-t border-teal-300" />
             <ul className="space-y-4">
               {suicideHotline?.phoneNumbers?.map((item, index) => (
                 <li key={index} className="flex items-start">
                   <span className="inline-block w-4 h-4 rounded-full bg-teal-300 mr-4 mt-1 flex-shrink-0"></span>
-                  <span className="text-white text-lg md:text-xl">{item.number}</span>
+                  <span className="text-white text-xl md:text-2xl">{item.number}</span>
                 </li>
               ))}
             </ul>
           </div>
         </div>
+        {localizedResources?.map((item, index) => (
+          <div key={index} className="pt-16">
+            <h3 className={cn(item.titleClasses)}>{item.title}</h3>
+            <RichText
+              data={item.description}
+              enableProse={false}
+              enableGutter={false}
+              className={cn(item.descriptionClasses)}
+            />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16">
+              {item.resources?.map((resource, index) => (
+                <div key={index} className="flex items-center gap-4 border-b border-teal-300 pb-4">
+                  <Media resource={resource.icon} alt={resource.text} className="w-6 h-6" />
+                  <Link href={resource.link} className="text-white text-xl md:text-2xl">
+                    {resource.text}
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   )
--- src/blocks/EmergenciesBlock/config.ts
+++ src/blocks/EmergenciesBlock/config.ts
@@ -25,103 +25,288 @@ export const EmergenciesBlock: Block = {
       },
     }),
     {
-      name: 'title',
-      type: 'text',
-      required: true,
-      label: 'Title',
-      defaultValue: 'Emergencies',
-    },
-    textClasses({
-      overrides: {
-        name: 'titleClasses',
-        defaultValue: ['text-4xl', 'font-semibold', 'text-white', 'md:text-center', 'mb-12'],
-      },
-    }),
-    {
-      name: 'mainText',
-      type: 'richText',
-      required: true,
-      label: 'Main Text',
-    },
-    textClasses({
-      overrides: {
-        name: 'mainTextClasses',
-        defaultValue: [
-          'text-xl',
-          'md:text-2xl',
-          'text-white',
-          'md:text-center',
-          'mb-16',
-          'lg:w-7/10',
-          'w-4/5',
-        ],
-      },
-    }),
-    {
-      name: 'emergencyCases',
-      type: 'group',
-      label: 'Emergency Cases',
+      label: 'Intro Text',
+      type: 'collapsible',
       fields: [
         {
           name: 'title',
           type: 'text',
           required: true,
           label: 'Title',
-          defaultValue: 'Sample Emergency Cases',
+          defaultValue: 'Emergency and Support <br /> Resources',
         },
         textClasses({
           overrides: {
             name: 'titleClasses',
-            defaultValue: ['text-xl', 'md:text-2xl', 'lg:text-3xl', 'font-semibold', 'text-white'],
+            defaultValue: [
+              'text-3xl',
+              'md:text-4xl',
+              'lg:text-5xl',
+              'font-semibold',
+              'text-white',
+              'md:text-center',
+              'mb-12',
+            ],
           },
         }),
         {
-          name: 'cases',
-          type: 'array',
+          name: 'mainText',
+          type: 'richText',
           required: true,
-          minRows: 1,
-          label: 'Cases',
-          fields: [
-            {
-              name: 'text',
-              type: 'text',
-              required: true,
-              label: 'Case Text',
+          label: 'Main Text',
+          defaultValue: {
+            root: {
+              children: [
+                {
+                  children: [
+                    {
+                      detail: 0,
+                      format: 0,
+                      mode: 'normal',
+                      style: '',
+                      text: 'Evergreen Psychiatry Group offers outpatient clinic services only with limited office hours. Patients are seen on an appointment basis only. In case of an emergency, please browse our emergency resources to find a service that suits your needs. ',
+                      type: 'text',
+                      version: 1,
+                    },
+                  ],
+                  direction: 'ltr',
+                  format: '',
+                  indent: 0,
+                  type: 'paragraph',
+                  version: 1,
+                  textFormat: 0,
+                  textStyle: '',
+                },
+              ],
+              direction: 'ltr',
+              format: '',
+              indent: 0,
+              type: 'root',
+              version: 1,
             },
-          ],
+          },
         },
+        textClasses({
+          overrides: {
+            name: 'mainTextClasses',
+            defaultValue: [
+              'text-xl',
+              'md:text-2xl',
+              'text-white',
+              'md:text-center',
+              'mb-16',
+              'lg:w-7/10',
+              'w-4/5',
+              'mx-auto',
+            ],
+          },
+        }),
+        {
+          name: 'subtitle',
+          type: 'text',
+          required: true,
+          defaultValue: 'Emergencies',
+        },
+        textClasses({
+          overrides: {
+            name: 'subtitleClasses',
+            defaultValue: ['text-2xl', 'md:text-3xl', 'font-semibold', 'text-white', 'mb-12'],
+          },
+        }),
+        {
+          name: 'subText',
+          type: 'richText',
+          required: true,
+          label: 'Sub Text',
+          defaultValue: {
+            root: {
+              children: [
+                {
+                  children: [
+                    {
+                      detail: 0,
+                      format: 0,
+                      mode: 'normal',
+                      style: '',
+                      text: 'Evergreen Psychiatry Group offers outpatient clinic services only with limited office hours. Patients are seen On An Appointment Basis Only. ',
+                      type: 'text',
+                      version: 1,
+                    },
+
+                    {
+                      detail: 0,
+                      format: 1,
+                      mode: 'normal',
+                      style: '',
+                      text: 'If you are suicidal, It is important that you contact 911 and Go to the Emergency Room Immediately.',
+                      type: 'text',
+                      version: 1,
+                    },
+                  ],
+                  direction: 'ltr',
+                  format: '',
+                  indent: 0,
+                  type: 'paragraph',
+                  version: 1,
+                  textFormat: 0,
+                  textStyle: '',
+                },
+              ],
+              direction: 'ltr',
+              format: '',
+              indent: 0,
+              type: 'root',
+              version: 1,
+            },
+          },
+        },
+        textClasses({
+          overrides: {
+            name: 'subTextClasses',
+            defaultValue: ['text-xl', 'md:text-2xl', 'text-white', 'mb-12'],
+          },
+        }),
       ],
     },
     {
-      name: 'suicideHotline',
-      type: 'group',
-      label: 'Suicide Prevention Hotline',
+      type: 'collapsible',
+      label: 'Data',
       fields: [
         {
-          name: 'title',
-          type: 'text',
-          required: true,
-          label: 'Title',
-          defaultValue: 'National Suicide Prevention Lifeline',
-        },
-        textClasses({
-          overrides: {
-            name: 'titleClasses',
-            defaultValue: ['text-xl', 'md:text-2xl', 'lg:text-3xl', 'font-semibold', 'text-white'],
-          },
-        }),
-        {
-          name: 'phoneNumbers',
-          type: 'array',
-          required: true,
-          minRows: 1,
-          label: 'Phone Numbers',
+          name: 'emergencyCases',
+          type: 'group',
+          label: 'Emergency Cases',
           fields: [
             {
-              name: 'number',
+              name: 'title',
               type: 'text',
               required: true,
-              label: 'Phone Number',
+              label: 'Title',
+              defaultValue: 'Sample Emergency Cases',
+            },
+            textClasses({
+              overrides: {
+                name: 'titleClasses',
+                defaultValue: [
+                  'text-xl',
+                  'md:text-2xl',
+                  'lg:text-3xl',
+                  'font-semibold',
+                  'text-white',
+                ],
+              },
+            }),
+            {
+              name: 'cases',
+              type: 'array',
+              required: true,
+              minRows: 1,
+              label: 'Cases',
+              fields: [
+                {
+                  name: 'text',
+                  type: 'text',
+                  required: true,
+                  label: 'Case Text',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: 'suicideHotline',
+          type: 'group',
+          label: 'Suicide Prevention Hotline',
+          fields: [
+            {
+              name: 'title',
+              type: 'text',
+              required: true,
+              label: 'Title',
+              defaultValue: 'National Suicide Prevention Lifeline',
+            },
+            textClasses({
+              overrides: {
+                name: 'titleClasses',
+                defaultValue: [
+                  'text-xl',
+                  'md:text-2xl',
+                  'lg:text-3xl',
+                  'font-semibold',
+                  'text-white',
+                ],
+              },
+            }),
+            {
+              name: 'phoneNumbers',
+              type: 'array',
+              required: true,
+              minRows: 1,
+              label: 'Phone Numbers',
+              fields: [
+                {
+                  name: 'number',
+                  type: 'text',
+                  required: true,
+                  label: 'Phone Number',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: 'localizedResources',
+          type: 'array',
+          fields: [
+            {
+              name: 'title',
+              type: 'text',
+              required: true,
+              label: 'Title',
+              defaultValue: 'County Name',
+            },
+            textClasses({
+              overrides: {
+                name: 'titleClasses',
+                defaultValue: ['text-2xl', 'md:text-3xl', 'font-semibold', 'text-white', 'mb-8'],
+              },
+            }),
+            {
+              name: 'description',
+              type: 'richText',
+              required: true,
+              label: 'Description',
+            },
+            textClasses({
+              overrides: {
+                name: 'descriptionClasses',
+                defaultValue: ['text-xl', 'md:text-2xl', 'text-white', 'font-light', 'mb-6'],
+              },
+            }),
+            {
+              name: 'resources',
+              type: 'array',
+              required: true,
+              fields: [
+                {
+                  name: 'icon',
+                  type: 'upload',
+                  required: true,
+                  relationTo: 'media',
+                },
+                {
+                  name: 'link',
+                  type: 'text',
+                  required: true,
+                  label: 'Link',
+                },
+                {
+                  name: 'text',
+                  type: 'text',
+                  required: true,
+                  label: 'Text',
+                },
+              ],
             },
           ],
         },
